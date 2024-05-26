cab59b47a8348f93f3b2cd038b06b4c670ac84ed
karimsh2001 <karimsh2001@gmail.com>
2024-05-26 16:23:29 -0400

    limpieza

---
+++ src/paginas/pagina1.js
@@ -0,0 +1,9 @@
+function Pagina1() {
+    return (
+      <div className="App">
+        <h1>hola mundo 1</h1>
+      </div>
+    );
+  }
+  
+  export default Pagina1;
---
+++ src/paginas/pagina2.js
@@ -0,0 +1,9 @@
+function Pagina2() {
+    return (
+      <div className="App">
+        <h1>hola mundo 2</h1>
+      </div>
+    );
+  }
+  
+  export default Pagina2;
