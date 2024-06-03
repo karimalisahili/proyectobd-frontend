9c032cdda11f9569c673a74adf1b63bc509068df
Sarce18 <barretocesar33rgmail.com>
2024-06-02 22:36:37 -0400

    Migracion de Frontend a Vite mas Creacion de pantalla Login

--- src/paginas/pagina1.js
+++
@@ -1,9 +0,0 @@
-function Pagina1() {
-    return (
-      <div className="App">
-        <h1>hola mundo 1</h1>
-      </div>
-    );
-  }
-  
-  export default Pagina1;
--- src/paginas/pagina2.js
+++
@@ -1,9 +0,0 @@
-function Pagina2() {
-    return (
-      <div className="App">
-        <h1>hola mundo 2</h1>
-      </div>
-    );
-  }
-  
-  export default Pagina2;
