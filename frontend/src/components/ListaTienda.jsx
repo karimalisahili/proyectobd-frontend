7e15c60453dd61c0beb36059d9a4fa374eb91215
Sarce18 <barretocesar33rgmail.com>
2024-07-07 20:19:01 -0400

    coño de la madre

--- frontend/src/components/ListaTienda.jsx
+++ frontend/src/components/ListaTienda.jsx
@@ -307,10 +307,6 @@ function Ventas() {
 
   const [activeStep, setActiveStep] = React.useState(0);
   const [skipped, setSkipped] = React.useState(new Set());
-
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
     
   const handleNext = () => {
     let newSkipped = skipped;
@@ -739,4 +735,4 @@ ListaTienda.propTypes = {
   };
 
   export default ListaTienda;
-  
+   
